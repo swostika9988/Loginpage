b3e03fa1ab868837ec79bc4bdc828ce542daa06b
swostika9988 <sostikamahato40@gmail.com>
2023-10-06 12:06:18 +0545

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Login.css"
 
 function Login() {
+  const [email,setEmail]= useState("");
+  const [password,setPassword]= useState("");
+  const [allEntry, setAllEntry] = useState([]);
+  const submitForm=(e)=>{
+    e.preventDefault();
+    const newEntry={email:email,password:password};
+    setAllEntry([...allEntry,newEntry]);
+    console.log(allEntry);
+  }
   return (
-    <div>Login</div>
+    <>
+     <form action=""onSubmit={submitForm}>
+      <h1>LoginForm</h1>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" name='email' id='email' placeholder='example@gmail.com' autoComplete='off' 
+        value={email} 
+        onChange={(e)=>setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name='password' id='password' placeholder='*****' autoComplete='off'
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        />
+      </div>
+      <button type='submit'>Login</button>
+     </form>
+     
+    </>
   )
 }
 
